22940809cab2f29d0f01fdda7c3023209fbeef72
Harsha Amarasinghe <gm.harsha.amarasinghe@gmail.com>
2023-05-18 23:32:14 +0530

    home

--- Minimal Fitness/Views/Daily Views/viewHome.swift
+++ Minimal Fitness/Views/Daily Views/viewHome.swift
@@ -36,30 +36,32 @@ class viewHome: UIViewController {
     ]
     //Components
     
-    let labelHello : UILabel = {
+    let labelTitleOne : UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 36, weight: .thin)
-        label.textColor = UIColor(red: 89/255, green: 89/255, blue: 89/255, alpha: 1.0)
+        //label.textColor = UIColor(red: 89/255, green: 89/255, blue: 89/255, alpha: 1.0)
         label.text = "Let's thrive"
         label.textAlignment = .center
         return label
     }()
     
-    let labelName : UILabel = {
+    let labelTitleTwo : UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .systemFont(ofSize: 28, weight: .bold)
+        label.font = .systemFont(ofSize: 14, weight: .thin)
         label.textColor = UIColor(red: 89/255, green: 89/255, blue: 89/255, alpha: 1.0)
-        //label.text = "Hello"
+        label.text = "It’s our leg day"
         label.textAlignment = .center
         return label
     }()
     
     let imageProfile: UIImageView = {
-        let imageView = UIImageView(image: UIImage(named: "profile-man"))
+        let imageView = UIImageView(image: UIImage(named: "man"))
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 40
         return imageView
     }()
     
@@ -82,8 +84,22 @@ class viewHome: UIViewController {
         table.backgroundColor = .systemBackground
         table.allowsSelection = true
         table.register(CustomCell.self, forCellReuseIdentifier: CustomCell.identifier)
+//        table.layer.borderWidth = 1
+//        table.layer.borderColor = CGColor(red: 255, green: 255, blue: 255, alpha: 1.0)
+        table.layer.cornerRadius = 8
+        table.backgroundColor = UIColor(red: 208/255, green: 208/255, blue: 208/255, alpha: 1.0)
+        
         return table
     }()
+    
+    
+    let image: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "home"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     
     //Lifecycle
     
@@ -100,48 +116,82 @@ class viewHome: UIViewController {
     
     func setupUI(){
         view.backgroundColor = .white
-        view.addSubview(labelHello)
-        
-        labelName.text = userName
-        
-        view.addSubview(labelName)
+        view.addSubview(labelTitleOne)
+        view.addSubview(labelTitleTwo)
         view.addSubview(imageProfile)
-        view.addSubview(labelTitle)
         view.addSubview(exerciseTable)
+        view.addSubview(image)
+        
+//        labelName.text = userName
+//
+//        view.addSubview(labelName)
+//        view.addSubview(imageProfile)
+//        view.addSubview(labelTitle)
+//        view.addSubview(exerciseTable)
         
         //Constraints
         
-        labelHello.snp.makeConstraints { make in
+        labelTitleOne.snp.makeConstraints { make in
             make.top.equalTo(view.snp.top).offset(70)
             make.left.equalTo(view.safeAreaLayoutGuide.snp.left).offset(25)
         }
         
-        labelName.snp.makeConstraints { make in
-            make.top.equalTo(view.snp.top).offset(70)
-            make.left.equalTo(labelHello.snp.right).offset(10)
+        labelTitleTwo.snp.makeConstraints { make in
+            make.top.equalTo(labelTitleOne.snp.bottom).offset(5)
+            make.left.equalTo(view.safeAreaLayoutGuide.snp.left).offset(25)
         }
         
         imageProfile.snp.makeConstraints { make in
-            make.top.equalTo(view.snp.top).offset(50)
-            make.right.equalTo(view.safeAreaLayoutGuide.snp.right).offset(-25)
-            make.size.equalTo(CGSize(width: 60, height: 60))
-        }
-        
-        labelTitle.snp.makeConstraints { make in
-            make.top.equalTo(view.snp.top).offset(120)
-            //make.right.equalTo(view.safeAreaLayoutGuide.snp.right).offset(-25)
-            make.centerX.equalTo(view)
-            make.size.equalTo(CGSize(width: 340, height: 50))
+            make.top.equalTo(view.snp.top).offset(60)
+            //make.left.equalTo(labelTitleOne.snp.right).offset(25)
+            make.right.equalTo(view.snp.right).offset(-25)
+            make.size.equalTo(CGSize(width: 80, height: 80))
         }
         
         exerciseTable.snp.makeConstraints { make in
-            make.top.equalTo(labelTitle.snp.bottom).offset(20)
-            make.bottom.equalTo(view.snp.bottom).offset(-10)
-            make.left.equalTo(view.safeAreaLayoutGuide.snp.right).offset(20)
+            make.top.equalTo(labelTitleTwo.snp.bottom).offset(40)
+            //make.bottom.equalTo(view.snp.bottom).offset(-10)
+            make.left.equalTo(view.safeAreaLayoutGuide.snp.left).offset(20)
             make.right.equalTo(view.safeAreaLayoutGuide.snp.right).offset(-20)
             make.centerX.equalTo(view)
-            make.size.equalTo(CGSize(width: 340, height: 50))
+            make.size.equalTo(CGSize(width: 340, height: 340))
         }
+        
+        
+        image.snp.makeConstraints { make in
+            make.top.equalTo(exerciseTable.snp.bottom).offset(20)
+            make.left.equalTo(view.safeAreaLayoutGuide.snp.left).offset(-20)
+            //make.centerX.equalTo(view)
+            make.size.equalTo(CGSize(width: 350, height: 350))
+        }
+        
+        
+//        labelName.snp.makeConstraints { make in
+//            make.top.equalTo(view.snp.top).offset(70)
+//            make.left.equalTo(labelHello.snp.right).offset(10)
+//        }
+//
+//        imageProfile.snp.makeConstraints { make in
+//            make.top.equalTo(view.snp.top).offset(50)
+//            make.right.equalTo(view.safeAreaLayoutGuide.snp.right).offset(-25)
+//            make.size.equalTo(CGSize(width: 60, height: 60))
+//        }
+//
+//        labelTitle.snp.makeConstraints { make in
+//            make.top.equalTo(view.snp.top).offset(120)
+//            //make.right.equalTo(view.safeAreaLayoutGuide.snp.right).offset(-25)
+//            make.centerX.equalTo(view)
+//            make.size.equalTo(CGSize(width: 340, height: 50))
+//        }
+//
+//        exerciseTable.snp.makeConstraints { make in
+//            make.top.equalTo(labelTitle.snp.bottom).offset(20)
+//            make.bottom.equalTo(view.snp.bottom).offset(-10)
+//            make.left.equalTo(view.safeAreaLayoutGuide.snp.right).offset(20)
+//            make.right.equalTo(view.safeAreaLayoutGuide.snp.right).offset(-20)
+//            make.centerX.equalTo(view)
+//            make.size.equalTo(CGSize(width: 340, height: 50))
+//        }
     }
 }
 
@@ -161,6 +211,8 @@ extension viewHome : UITableViewDelegate, UITableViewDataSource{
         
         cell.configure(with: image, and: label)
         
+        cell.contentView.backgroundColor = UIColor(red: 236/255, green: 236/255, blue: 236/255, alpha: 1.0)
+        
         return cell
         
     }
@@ -175,4 +227,7 @@ extension viewHome : UITableViewDelegate, UITableViewDataSource{
         let vc = viewExercise()
         navigationController?.pushViewController(vc, animated: true)
     }
+    
+    
+    
 }
